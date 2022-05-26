add547e8c3041b13140130e6c8cf7e43c3be5105
Miftahur23 <miftahur109970@example.com>
2022-05-26 01:12:54 +0000

    student create and read

---
+++ resources/views/admin/fixed/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <a class="nav-link" href="{{route('students.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Students
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('loginpage');
+// });
+
+
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/login', 'loginpage')->name('loginpage');
+
+Route::post('users/logging_in', [UserController::class, 'doLogin'])->name('login');
+Route::get('users/logging_out', [UserController::class, 'doLogout'])->name('logout');
+
+Route::resource('students', StudentController::class);
+
